6f69c708043790523378fb2212417cd28b3ba806
sadumina2003 <153711766+sadumina2003@users.noreply.github.com>
2024-11-23 15:01:20 +0530

    connect to mongoDb

--- project mern/api/index.js
+++ project mern/api/index.js
@@ -1,7 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import { error } from 'console';
+
+dotenv.config();
 
 const app = express();
 
+mongoose.connect(process.env.MONGO)
+.then(()=> {
+
+console.log("Connect to the MONOGODB")
+
+})
+.catch((err) =>{
+
+    console.log(err);
+})
 app.listen(5000 , ()=>{
 
 
